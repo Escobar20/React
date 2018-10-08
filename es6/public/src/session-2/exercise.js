3a8311817759b025a5b9a5aa07d9fafe9f00b036
Escobedo Escareno <m.escobedo.escareno@accenture.com>
2018-10-08 12:56:35 -0500

    Remove stuff

--- es6/public/src/session-2/exercise.js
+++ es6/public/src/session-2/exercise.js
@@ -1,18 +1,3 @@
-import { average } from './spread-operator';
-
-export const standardDeviation = (...sample) => {
-    let sd = 0;
-
-    if (sample.length > 0) {
-        let avg = average(...sample);
-
-        let deviation = sample.map(v => Math.pow(v - avg, 2))
-
-        sd = Math.sqrt(average(...deviation));
-    }
-    return sd;
-}
-
 export const sports = ['football', 'basketball', 'baseball', 'voleyball', 'hockey', 'chess', 'texas poker'];
 export const numbers = [0, 'f', {}, NaN, Infinity];
 export const clauses = {
